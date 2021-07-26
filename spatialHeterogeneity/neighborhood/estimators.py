45bda90a72121159b8e246f71185efd1b06f49f0
Adriano Martinelli <art@zurich.ibm.com>
2021-07-26 18:43:22 +0200

    add docstrings [skip ci]

--- spatialHeterogeneity/neighborhood/estimators.py
+++ spatialHeterogeneity/neighborhood/estimators.py
@@ -7,11 +7,29 @@ from .base_estimators import Interactions, _infiltration, RipleysK
 from .utils import get_node_interactions
 from ..utils.general import is_categorical
 
-
 # %%
-def interactions(so, spl, attr, mode='classic', prediction_type='observation', *, n_permutations=100,
-                 random_seed=None, alpha=.01, try_load=True, key_added=None, graph_key='knn',
-                 inplace=True):
+def interactions(so, spl: str, attr: str, mode: str ='classic', prediction_type: str ='observation', *, n_permutations: int =100,
+                 random_seed=None, alpha: float =.01, try_load: bool =True, key_added: str =None, graph_key: str ='knn',
+                 inplace: bool =True) -> None:
+    """Compute interaction strength between species.
+
+    Args:
+        so: SpatialOmics instance
+        spl: Spl for which to compute the metric
+        attr: Categorical feature in SpatialOmics.obs to use for the grouping
+        mode: One of {classic, histoCAT, proportion}, see notes
+        n_permutations: Number of permutations to compute p-values and the interactions strength score (mode diff)
+        random_seed: Random seed for permutations
+        alpha: Threshold for significance
+        prediction_type: One of {observation, pvalue, diff}, see Notes
+        try_load: load pre-computed permutation results if available
+        key_added: Key added to SpatialOmics.uns[spl][metric][key_added]
+        graph_key: Specifies the graph representation to use in so.G[spl] if `local=True`.
+        inplace: Whether to add the metric to the current SpatialOmics instance or to return a new one.
+
+    Returns:
+
+    """
     so = so if inplace else so.copy()
 
     # NOTE: uns_path = f'{spl}/interactions/'
@@ -34,8 +52,23 @@ def interactions(so, spl, attr, mode='classic', prediction_type='observation', *
         return so
 
 
-def infiltration(so, spl, attr, *, interaction1=('tumor', 'immune'), interaction2=('immune', 'immune'),
-                 add_key='infiltration', inplace=True, graph_key='knn'):
+def infiltration(so, spl: str, attr: str, *, interaction1=('tumor', 'immune'), interaction2=('immune', 'immune'),
+                 add_key='infiltration', inplace=True, graph_key='knn') -> None:
+    """Compute infiltration score.
+
+    Args:
+        so: SpatialOmics instance
+        spl: Spl for which to compute the metric
+        attr: Categorical feature in SpatialOmics.obs to use for the grouping
+        interaction1: labels of enumerator interaction
+        interaction2: labels of denominator interaction
+        key_added: Key added to SpatialOmics.uns[spl][metric][key_added]
+        inplace: Whether to add the metric to the current SpatialOmics instance or to return a new one.
+        graph_key: Specifies the graph representation to use in so.G[spl] if `local=True`.
+
+    Returns:
+
+    """
     so = so if inplace else so.copy()
 
     data = so.obs[spl][attr]
@@ -60,8 +93,28 @@ def infiltration(so, spl, attr, *, interaction1=('tumor', 'immune'), interaction
         return so
 
 
-def ripleysK(so, spl, attr, id, *, mode='K', radii=None, correction='ripley', inplace=True, key_added=None,
-             graph_key='knn'):
+def ripleysK(so, spl: str, attr: str, id, *, mode='K', radii=None, correction='ripley', inplace=True, key_added=None):
+    """Compute Ripley's K
+
+    Args:
+        so: SpatialOmics instance
+        spl: Spl for which to compute the metric
+        attr: Categorical feature in SpatialOmics.obs to use for the grouping
+        id: The category in the categorical feature `attr`, for which Ripley's K should be computed
+        mode: {K, csr-deviation}. If `K`, Ripley's K is estimated, with `csr-deviation` the deviation from a poission process is computed.
+        radii: List of radiis for which Ripley's K is computed
+        correction: Correction method to use to correct for boarder effects, see [1].
+        inplace: Whether to add the metric to the current SpatialOmics instance or to return a new one.
+        key_added: Key added to SpatialOmics.uns[spl][metric][key_added]
+        graph_key: Specifies the graph representation to use in so.G[spl] if `local=True`.
+
+    Returns:
+        Ripley's K estimates
+
+    Notes:
+        .. [1] https://docs.astropy.org/en/stable/stats/ripley.html
+
+    """
     so = so if inplace else so.copy()
 
     # NOTE: uns_path = f'{spl}/clustering/'
@@ -78,7 +131,7 @@ def ripleysK(so, spl, attr, id, *, mode='K', radii=None, correction='ripley', in
         return so
 
 
-def add2uns(so, res, spl, parent_key, key_added):
+def add2uns(so, res, spl: str, parent_key, key_added):
     if spl in so.uns:
         if parent_key in so.uns[spl]:
             so.uns[spl][parent_key][key_added] = res
