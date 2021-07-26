c9f1053a705e8ec4606af95cba9d63036bdab91a
Adriano Martinelli <art@zurich.ibm.com>
2021-07-26 11:54:04 +0200

    docstring decorator [skip ci]

--- spatialHeterogeneity/utils/docs/default_docs.py
+++ spatialHeterogeneity/utils/docs/default_docs.py
@@ -1,4 +1,6 @@
-def ddoc(fun, key_added='DEFAULT'):
+def ddoc(fun):
+    key_added = 'DEFAULT'
+
     doc = fun.__doc__
 
     # replace so
@@ -8,10 +10,28 @@ def ddoc(fun, key_added='DEFAULT'):
     doc = doc.replace('spl:', 'spl: sample for which to apply the function')
 
     # replace key_added
-    doc = doc.repalce('key_added', f'key_added: key added to {key_added}')
+    doc = doc.replace('key_added', f'key_added: key added to {key_added}')
 
     # replace inplace
-    doc = doc.repalce('inplace:', 'inplace: whether to return a new SpatialOmics instance')
+    doc = doc.replace('inplace:', 'inplace: whether to return a new SpatialOmics instance')
 
     # replace doc
     fun.__doc__ = doc
+
+    return fun
+
+from docrep import DocstringProcessor
+docstrings = DocstringProcessor()
+
+@docstrings.get_sections
+def default_doc(so, inplace):
+    """
+
+    Args:
+        so: SpatialOmics
+        inplace: Apply function in place
+
+    Returns:
+
+    """
+    pass
