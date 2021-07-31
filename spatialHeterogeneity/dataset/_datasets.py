bd166987af0ecb57a4904f6f61066aecf00795bc
Adriano Martinelli <art@zurich.ibm.com>
2021-07-31 12:27:50 +0200

    change url [skip ci]

--- spatialHeterogeneity/dataset/_datasets.py
+++ spatialHeterogeneity/dataset/_datasets.py
@@ -2,6 +2,6 @@ from ._utils import DataSet
 
 imc = DataSet(
     name='imc',
-    url='https://ndownloader.figshare.com/files/28974720',
+    url='https://ndownloader.figshare.com/files/29000478',
     doc_header='Pre-processed subset IMC dataset from `Jackson et al '
 )
