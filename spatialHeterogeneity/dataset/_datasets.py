916d2706af4d99d8d6eeef5ad9cd859577bd4468
Adriano Martinelli <art@zurich.ibm.com>
2022-02-23 16:33:54 -0500

    add mibi datasets

--- spatialHeterogeneity/dataset/_datasets.py
+++ spatialHeterogeneity/dataset/_datasets.py
@@ -5,3 +5,15 @@ imc = DataSet(
     url='https://figshare.com/ndownloader/files/31750769',
     doc_header='Pre-processed subset IMC dataset from `Jackson et al '
 )
+
+mibi = DataSet(
+    name='mibi',
+    url='https://figshare.com/ndownloader/files/31750769',
+    doc_header='Processed data from https://www.angelolab.com/mibi-data and https://mibi-share.ionpath.com/tracker/imageset'
+)
+
+mibi_pop = DataSet(
+    name='mibi',
+    url='https://figshare.com/ndownloader/files/31750769',
+    doc_header='Processed and populated data (graphs, metrics) from https://www.angelolab.com/mibi-data and https://mibi-share.ionpath.com/tracker/imageset'
+)
