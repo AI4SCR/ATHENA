85b87a8efbc2eec3020889e66d795c8be5ea3400
Santiago Manuel Castro Dau <Santiago.Castro.Dau@ibm.com>
2022-11-07 13:37:45 +0100

    Commented graphBuilder.py

--- athena/graph_builder/graphBuilder.py
+++ athena/graph_builder/graphBuilder.py
@@ -25,30 +25,48 @@ def build_graph(so, spl: str, builder_type='knn', mask_key='cellmasks', key_adde
 
     .. _dilation: https://scikit-image.org/docs/stable/api/skimage.morphology.html#skimage.morphology.binary_dilation
     """
+
+    # Raise error is the builder_type is invalid
     if builder_type not in GRAPH_BUILDERS:
         raise ValueError(f'invalid type {builder_type}. Available types are {GRAPH_BUILDERS.keys()}')
+
+    # Get default bulding parameters if non are specified    
     if config is None:
         config = GRAPH_BUILDER_DEFAULT_PARAMS[builder_type].copy()
+
+    # If no title for the graph is provided use builder_type
+    # TODO: make default name also include the cell types involved    
     if key_added is None:
         key_added = builder_type
 
+    # If no masks are provided check that builder_type != 'contact'. Then build
     if mask_key is None:
+        # Raise error if no masks were provided and the builder_type is contact
         if builder_type == 'contact':
             raise ValueError(
                 'Contact-graph requires segmentations masks. To compute a contact graph please specify `the mask_key` to use in so.masks[spl]')
+        
+        # Get coordinates for every cell in the sample and build graph
         ndat = so.obs[spl][[coordinate_keys[0], coordinate_keys[1]]]
         builder = GRAPH_BUILDERS[builder_type](config)
         g = builder(ndata=ndat)
     else:
+        # Get masks and build graph
         mask = so.get_mask(spl, mask_key)
         g = GRAPH_BUILDERS[builder_type].from_mask(config, mask)
 
+    # If include self is specified and true and the graph built is a contact graph, add self edges in graph. 
     if 'include_self' in config['builder_params'] and config['builder_params'][
         'include_self'] and builder_type == 'contact':
         edge_list = [(i, i) for i in g.nodes]
         g.add_edges_from(edge_list)
 
+    # I would just rewrite: if not inplace: so.copy()
+    # TODO: imporve readability here. 
     so = so if inplace else so.copy()
+
+    # If there already is a graph from spl then add or update the so object with graph at key_added
+    # TODO: here maybe the if/else is not needed. 
     if spl in so.G:
         so.G[spl].update({key_added: g})
     else:
