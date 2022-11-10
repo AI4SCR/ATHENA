41540f439a1a9c8789c2da081e64180cd5ade7fc
Santiago Manuel Castro Dau <Santiago.Castro.Dau@ibm.com>
2022-11-10 14:07:16 +0100

    Added code for debuging possible errors when building sub set graphs.

--- athena/graph_builder/graphBuilder.py
+++ athena/graph_builder/graphBuilder.py
@@ -4,14 +4,14 @@ import numpy as np
 
 def build_graph(so, 
                 spl: str, 
-                builder_type='knn', 
-                mask_key='cellmasks', 
-                key_added=None, 
-                config=None, 
-                inplace=True,
+                builder_type: str='knn', 
+                mask_key: str='cellmasks', 
+                key_added: str=None, 
+                config: dict=None, 
+                inplace: bool=True,
                 coordinate_keys=('x', 'y'),
-                col_name=None, 
-                types=None):
+                col_name: str=None, 
+                types: list=None):
     """Build graph representation for a sample. A graph is constructed based on the provided segmentation masks
     for the sample. For the `knn` and `radius` graph representation the centroid of each mask is used. For the `contact`
     graph representation the segmentation masks are dilation_ is performed. The segmentation masks that overlap after
@@ -23,14 +23,15 @@ def build_graph(so,
         builder_type: graph type to construct {knn, radius, contact}
         mask_key: key in so.masks[spl] to use as segmentation masks from which the observation
                 coordinates are extracted, if `None` `coordinate_keys` from `obs` attribute are used
-        key_added: key added in so.G[spl][key_add] to store the graph.
+        key_added: key added in so.G[spl][key_add] to store the graph. If not specified it defaluts to `builder_type`.
+                If the graph is being built on a subset of the nodes (e.g `col_name` and `types` are not None) 
+                then the key is `f'{builder_type} > {col_name} > {types}'`
         config: dict containing a dict 'builder_params' that specifies the graph construction parameters
         inplace: whether to return a new SpatialOmics instance
         coordinate_keys: column names of the x and y coordinates of a observation
         col_name: string of the column in so.obs[spl][col_name] which has the labels on which you want 
                 to subset the cells.
-        types: list of cell-types (strings) to be included in the graph. 
-                These strings should match those listed so.obs[spl][col_name].
+        types: list of stirngs which identify the labels in so.obs[spl][col_name] that should be included in the grapgh. 
                 If no list is provided the graph is built using all the cells/cell types. 
 
     Returns:
@@ -43,6 +44,10 @@ def build_graph(so,
     if builder_type not in GRAPH_BUILDERS:
         raise ValueError(f'invalid type {builder_type}. Available types are {GRAPH_BUILDERS.keys()}')
 
+    # Raise error if either `col_name` `types` is specified but not the other.
+    if (col_name is None) ^ (types is None):
+        raise NameError(f'failed to specify either `col_name` or `types`')
+
     # Get default bulding parameters if non are specified    
     if config is None:
         config = GRAPH_BUILDER_DEFAULT_PARAMS[builder_type].copy()
@@ -50,7 +55,10 @@ def build_graph(so,
     # If no title for the graph is provided use builder_type
     # TODO: make default name also include the cell types involved    
     if key_added is None:
-        key_added = builder_type
+        if types is not None:
+            key_added = f'{builder_type} > {col_name} > {types}'
+        else:
+            key_added = builder_type
 
     # If no masks are provided build graph with centroids.
     if mask_key is None:
@@ -60,9 +68,22 @@ def build_graph(so,
                 'Contact-graph requires segmentations masks. To compute a contact graph please specify `the mask_key` to use in so.masks[spl]')
         
         # If types is specified, get rid of coordinates that are in the out-set. 
-        # Else get all coordinates.
         if types is not None:
+            # Raise error if `col_name` is not found in 
+            if col_name not in so.obs[spl].columns:
+                raise NameError(f'{col_name} is not in so.obs[spl].columns')
+            
+            # Raise error if `types` is an empty list
+            if types == []:
+                raise NameError(f'types varaibel is empty. You need to give a non-empty list')
+
+            # Raise error if not all `types` have a match in `so.obs[spl][col_name].cat.categories.values`
+            if not np.all(np.isin(types, so.obs[spl][col_name].cat.categories.values)):
+                raise NameError(f'Not all elements provided in variable types are in so.obs[spl][col_name]')
+
+            # Finally, get coordinates
             ndat = so.obs[spl].query(f'{col_name} in @types')[coordinate_keys[0], coordinate_keys[1]]
+        # Else get all coordinates.
         else:
             ndat = so.obs[spl][[coordinate_keys[0], coordinate_keys[1]]]
 
@@ -96,4 +117,5 @@ def build_graph(so,
     if spl in so.G:
         so.G[spl].update({key_added: g})
     else:
+        # otherwise initialize new dictionary object at key `spl`
         so.G[spl] = {key_added: g}
