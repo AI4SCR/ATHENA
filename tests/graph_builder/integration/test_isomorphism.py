007703db637f96cd8f42a49303f4c0df5b779cbb
Santiago Manuel Castro Dau <Santiago.Castro.Dau@ibm.com>
2022-11-16 18:02:45 +0100

    Implemented sub graph isomorphic test.

---
+++ tests/graph_builder/integration/test_isomorphism.py
@@ -0,0 +1,40 @@
+import networkx as nx
+import athena as ath
+from athena.graph_builder.constants import GRAPH_BUILDER_DEFAULT_PARAMS
+import pytest
+
+# This tests wheather a sub set graph is sub graph isomorphic of the full graph. 
+def test_is_isomorphic():
+    # Loead data
+    so = ath.dataset.imc()
+
+    # Define sample
+    spl = 'slide_49_By2x5'
+
+    # Extrac centroids
+    ath.pp.extract_centroids(so, spl, mask_key='cellmasks')
+
+    # Build full graph with radius
+    builder_type = 'radius'
+    config = GRAPH_BUILDER_DEFAULT_PARAMS[builder_type]
+    config['builder_params']['radius'] = 20 # set radius
+    ath.graph.build_graph(so, spl, builder_type=builder_type, mask_key='cellmasks', config=config)
+
+    # Buil concept graph with radius
+    # Decide on subset
+    labels = ['endothelial']
+    filter_col = 'cell_type'
+
+    # Build subset graphs
+    # radius graph
+    config = GRAPH_BUILDER_DEFAULT_PARAMS[builder_type]
+    config['builder_params']['radius'] = 20 # set radius
+    ath.graph.build_graph(so, spl, builder_type='radius', mask_key='cellmasks', config=config,
+                        filter_col = filter_col,
+                        labels = labels)
+
+    A = so.G[spl][builder_type]  
+    B = so.G[spl][f'{builder_type} > {filter_col} > {labels}'] 
+
+    GM = nx.isomorphism.GraphMatcher(A, B)
+    assert GM.subgraph_is_isomorphic()
