2c951df01c4dbfa702f1eb809de0eecbeba59c26
Santiago Castro Dau <sanntiago5@gmail.com>
2023-10-13 16:54:24 +0200

    bug in knn and radius graph builder

--- athena/graph_builder/knn_graph_builder.py
+++ athena/graph_builder/knn_graph_builder.py
@@ -7,7 +7,6 @@ from .base_graph_builder import BaseGraphBuilder
 from ..utils.default_configs import EDGE_WEIGHT
 
 
-# %%
 class KNNGraphBuilder(BaseGraphBuilder):
     '''KNN (K-Nearest Neighbors) class for graph building.
     '''
@@ -27,7 +26,7 @@ class KNNGraphBuilder(BaseGraphBuilder):
 
     def __call__(self, so, spl):
         '''Build topology using a kNN algorithm based on the distance between the centroid of the nodes.
-        
+
         Args:
             so: spatial omic object
             spl: sting identifying the sample in the spatial omics object
@@ -51,7 +50,7 @@ class KNNGraphBuilder(BaseGraphBuilder):
             # If labels is specified, get rid of coordinates that are in the out-set. # Else get all coordinates.
             if self.config['build_concept_graph']:
                 # Get coordinates
-                ndata = so.obs[spl].query(f'{filter_col} in @labels')[coordinate_keys[0], coordinate_keys[1]]
+                ndata = so.obs[spl].query(f'{filter_col} in @labels')[[coordinate_keys[0], coordinate_keys[1]]]
             else:
                 ndata = so.obs[spl][[coordinate_keys[0], coordinate_keys[1]]]
 
@@ -66,18 +65,17 @@ class KNNGraphBuilder(BaseGraphBuilder):
                 cell_ids = so.obs[spl].query(f'{filter_col} in @labels').index.values
                 # Simplify masks filling it with 0s for cells that are not in `labels`
                 mask = np.where(np.isin(mask, cell_ids), mask, 0)
-            
+
             # Extract location:
             ndata = self.extract_location(mask)
-            
 
         # compute adjacency matrix
         adj = kneighbors_graph(ndata.to_numpy(), **self.config['builder_params'])
         df = pd.DataFrame(adj.A, index=ndata.index, columns=ndata.index)
         self.graph = nx.from_pandas_adjacency(df)  # this does not add the nodes in the same sequence as the index, column
 
-        # Puts node attribute (usually just coordinates) into dictionary 
-        # and then as node attributes in the graph. Set edge wheight to 1. 
+        # Puts node attribute (usually just coordinates) into dictionary
+        # and then as node attributes in the graph. Set edge weight to 1.
         attrs = df2node_attr(ndata)
         nx.set_node_attributes(self.graph, attrs)
         nx.set_edge_attributes(self.graph, 1, EDGE_WEIGHT)
--- athena/graph_builder/radius_graph_builder.py
+++ athena/graph_builder/radius_graph_builder.py
@@ -2,13 +2,11 @@ from sklearn.neighbors import radius_neighbors_graph
 import networkx as nx
 import pandas as pd
 import numpy as np
-
 from ..utils.tools.graph import df2node_attr
 from .base_graph_builder import BaseGraphBuilder
 from ..utils.default_configs import EDGE_WEIGHT
 
 
-# %%
 class RadiusGraphBuilder(BaseGraphBuilder):
     '''
     Radius graph class for graph building.
@@ -42,17 +40,17 @@ class RadiusGraphBuilder(BaseGraphBuilder):
             filter_col = self.config['concept_params']['filter_col']
             labels = self.config['concept_params']['labels']
             self.look_for_miss_specification_error(so, spl, filter_col, labels)
-        
+
         mask_key = self.config['mask_key']
         coordinate_keys = self.config['coordinate_keys']
 
         # If a cell subset is well are specified then simplify the mask
         if mask_key is None:
-            # If the subset is well specified (no error in `look_for_miss_specification_error`), 
-            # get rid of coordinates that are in the out-set. 
+            # If the subset is well specified (no error in `look_for_miss_specification_error`),
+            # get rid of coordinates that are in the out-set.
             if self.config['build_concept_graph']:
                 # Get coordinates
-                ndata = so.obs[spl].query(f'{filter_col} in @labels')[coordinate_keys[0], coordinate_keys[1]]
+                ndata = so.obs[spl].query(f'{filter_col} in @labels')[[coordinate_keys[0], coordinate_keys[1]]]
             # Else get all coordinates.
             else:
                 ndata = so.obs[spl][[coordinate_keys[0], coordinate_keys[1]]]
@@ -68,7 +66,7 @@ class RadiusGraphBuilder(BaseGraphBuilder):
                 cell_ids = so.obs[spl].query(f'{filter_col} in @labels').index.values
                 # Simplify masks filling it with 0s for cells that are not in `labels`
                 mask = np.where(np.isin(mask, cell_ids), mask, 0)
-            
+
             # Extract location:
             ndata = self.extract_location(mask)
 
@@ -77,8 +75,8 @@ class RadiusGraphBuilder(BaseGraphBuilder):
         df = pd.DataFrame(adj.A, index=ndata.index, columns=ndata.index)
         self.graph = nx.from_pandas_adjacency(df)  # this does not add the nodes in the same sequence as the index, column
 
-        # Puts node attribute (usually just coordinates) into dictionary 
-        # and then as node attributes in the graph. Set edge wheight to 1. 
+        # Puts node attribute (usually just coordinates) into dictionary
+        # and then as node attributes in the graph. Set edge weight to 1.
         attrs = df2node_attr(ndata)
         nx.set_node_attributes(self.graph, attrs)
         nx.set_edge_attributes(self.graph, 1, EDGE_WEIGHT)
