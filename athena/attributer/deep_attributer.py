80d5e9880a80c7fc14ceee0b915cceba319b0fc3
Santiago Castro Dau <sanntiago5@gmail.com>
2023-10-18 09:54:51 +0200

    class deepAttributer -> DeepAttributer

--- athena/attributer/deep_attributer.py
+++ athena/attributer/deep_attributer.py
@@ -1,9 +1,7 @@
 from .base_attributer import BaseAttributer
-import numpy as np
-import pandas as pd
-import networkx as nx
 
-class deepAttributer(BaseAttributer):
+
+class DeepAttributer(BaseAttributer):
 
     def __init__(self,
                  so,
@@ -11,24 +9,24 @@ class deepAttributer(BaseAttributer):
                  graph_key: str,
                  config: dict) -> None:
         """
-        Attributer class constructor. TODO: Compleate description, specify config structure. 
+        Attributer class constructor. TODO: Compleat description, specify config structure.
         """
         super().__init__(so, spl, graph_key, config)
 
     def __call__(self) -> None:
         """
-        Generates deep features and atributes them to nodes in so.G[spl][graph_key]. 
+        Generates deep features and attributes them to nodes in so.G[spl][graph_key].
 
         Returns:
             None. Attributes are saved to `so`.
         """
-        
+
         # TODO: implement this
         raise NotImplementedError('Not implemented yet.')
 
     def check_config(self) -> int:
         """
-        Check config integrity. TODO: Copleate description. 
+        Check config integrity. TODO: Compleat description.
         """
 
         # TODO: implement this
