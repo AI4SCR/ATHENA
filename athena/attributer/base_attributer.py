d19738d35c1cbd9cd0937214cff4de9612a253d4
Santiago Castro Dau <sanntiago5@gmail.com>
2023-10-18 10:09:08 +0200

    typo in base_attributer.py

--- athena/attributer/base_attributer.py
+++ athena/attributer/base_attributer.py
@@ -1,16 +1,17 @@
 from abc import ABC
 import abc
 
+
 class BaseAttributer(ABC):
-    
-    def __init__(self, 
+
+    def __init__(self,
                  so,
                  spl: str,
                  graph_key: str,
                  config: dict) -> None:
         """Base attributer constructor
         """
-        
+
         self.so = so
         self.spl = spl
         self.graph_key = graph_key
@@ -26,7 +27,7 @@ class BaseAttributer(ABC):
         # Get key from the first node
         any_node = list(self.so.G[self.spl][self.graph_key].nodes)[0]
 
-        # If the any_node ditionary (attrs) is not empty then clear all nodes. 
+        # If the any_node dictionary (attrs) is not empty then clear all nodes.
         if len(self.so.G[self.spl][self.graph_key].nodes[any_node]) > 0:
             for node in self.so.G[self.spl][self.graph_key].nodes:
                 self.so.G[self.spl][self.graph_key].nodes[node].clear()
